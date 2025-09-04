f69f77512ddbee0f589ec4168480183a300b2d22
Omkar-somawanshi <omkarsomwanshi552003@gmail.com>
2025-09-04 10:28:34 +0530

    fix: Correct POST route for sending messages in messageRoutes

--- Server/routes/messageRoutes.js
+++ Server/routes/messageRoutes.js
@@ -1,6 +1,5 @@
-// routes/messageRoutes.js
 import express from "express";
-import { protectRoute } from "../middleware/auth.js";   // <-- correct path
+import { protectRoute } from "../middleware/auth.js";
 import {
   getUsersForSidebar,
   getMessages,
@@ -12,7 +11,7 @@ const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
-router.post("/:id", protectRoute, sendMessage);
+router.post("/send/:id", protectRoute, sendMessage);  // <--- fixed here
 router.put("/seen/:id", protectRoute, markMessageAsSeen);
 
 export default router;
