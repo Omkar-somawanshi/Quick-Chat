9e2634a8ccda643d06367a7bfc0414c4ee051754
Omkar-somawanshi <omkarsomwanshi552003@gmail.com>
2025-09-17 11:27:16 +0530

    change

--- client/context/AuthProvider.jsx
+++ client/context/AuthProvider.jsx
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 import { AuthContext } from "./AuthContext.jsx";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+const backendUrl =  "https://quick-chat-sq4p.vercel.app/";
 axios.defaults.baseURL = backendUrl;
 
 export const AuthProvider = ({ children }) => {
